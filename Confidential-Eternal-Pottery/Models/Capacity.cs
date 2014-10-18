73e2b8002fd08688e7eb1e348466cc46a922d414
tojba <yougotspam123@gmail.com>
2014-10-18 18:05:56 +0200

    added enum and CU

---
+++ Confidential-Eternal-Pottery/Models/Capacity.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ConfidentialEternalPottery.Models
+{
+    public enum Capacity
+    {
+        two=2, three=3, five=5
+    }
+}
